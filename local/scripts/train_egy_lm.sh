c8998a9b1537653570316cb0beda1898ebae85de
maggieezzat <maggieezzat9@gmail.com>
2020-12-22 11:11:06 +0200

    fixed lm directories

--- local/scripts/train_egy_lm.sh
+++ local/scripts/train_egy_lm.sh
@@ -12,27 +12,16 @@ lm_test="data/local/lm/lm_corpus_test.txt"
 tri_dir="data/local/lm/trigram"
 
 lang_test_dir="data/lang_test"
+
 dict_dir_nosp="data/local/dict_nosp"
 #####################################################################
 
-
-
 mkdir -p $tri_dir
-
-################################################# Data Preparation ##################################################
-#append train transcripts to lm train data and generate words list
-#python local/scripts/gen_lm_data.py
-
-#sort unique the words list to avoid any duplicates
-#sort -u -o $lm_vocab $lm_vocab 
-#####################################################################################################################
-
 
 ################################################### LM Training #####################################################
 #trigram language model with limiting bi-gram and tri-gram counts
 echo "$0: Training trigram language model"
 $srilm_dir/ngram-count -text $lm_text -order 3 -limit-vocab -vocab $lm_vocab -unk -map-unk "<UNK>" -kndiscount -interpolate -lm $tri_dir/tri_lm.o3g.kn.gz -gt2min 5 -gt3min 3
-#$srilm_dir/ngram-count -text $lm_text -order 3 -limit-vocab -vocab $lm_vocab -unk -map-unk "<UNK>" -kndiscount -interpolate -lm $tri_dir/tri_lm.o3g.kn.gz
 
 
 #measure perplexity
@@ -41,6 +30,6 @@ $srilm_dir/ngram -unk -lm $tri_dir/tri_lm.o3g.kn.gz -ppl $lm_test -debug 0 >& $t
 
 
 #convert ARPA-format language models to FSTs.
-#echo  "$0: Converting language model to G.fst"
+echo  "$0: Converting language model to G.fst"
 utils/format_lm.sh data/lang $tri_dir/tri_lm.o3g.kn.gz $dict_dir_nosp/lexicon.txt $lang_test_dir
 #####################################################################################################################
