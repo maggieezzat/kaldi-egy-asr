887878c8d54f5511586b4826e410c3ead54da1f2
maggieezzat <maggieezzat9@gmail.com>
2020-12-31 16:32:24 +0200

    added script for decoding 1 hour

---
+++ decode_one_hr.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+data="dev1h"
+data_dir="data/"$data
+
+nnet3_affix=_tdnn_lstm_train
+
+cmd="run.pl"
+
+################################################## Feature Extraction ###############################################
+sort -u -o $data_dir/wav.scp $data_dir/wav.scp
+sort -u -o $data_dir/text $data_dir/text
+sort -u -o $data_dir/utt2spk $data_dir/utt2spk
+
+#make spk2utt file
+utils/utt2spk_to_spk2utt.pl $data_dir/utt2spk > $data_dir/spk2utt
+
+#fix data directory
+utils/fix_data_dir.sh $data_dir
+
+log_dir="$data_dir/log"
+mfcc_dir="$data_dir/mfcc"
+vad_dir="$data_dir/vad"
+segments_dir="$data_dir/segments"
+cmvn_dir="$data_dir/cmvn"
+steps/make_mfcc.sh --mfcc-config conf/mfcc.conf --nj $nj --cmd "$cmd" --write-utt2num-frames true $data_dir $log_dir $mfcc_dir || exit 1;
+
+#fix data directory
+utils/fix_data_dir.sh $data_dir 
+
+#compute cepstral mean and variance statistics per speaker.
+steps/compute_cmvn_stats.sh $data_dir $log_dir $cmvn_dir || exit 1;
+
+#fix data directory
+utils/fix_data_dir.sh $data_dir
+
+utils/copy_data_dir.sh $data_dir ${data_dir}_hires
+
+#high res features
+steps/make_mfcc.sh --nj $nj --mfcc-config conf/mfcc_hires.conf --cmd "$cmd" ${data_dir}_hires
+steps/compute_cmvn_stats.sh ${data_dir}_hires
+utils/fix_data_dir.sh ${data_dir}_hires
+
+#ivectors
+nspk=$(wc -l <${data_dir}_hires/spk2utt)
+steps/online/nnet2/extract_ivectors_online.sh --cmd "$cmd" --nj "${nspk}" \
+      ${data_dir}_hires exp/nnet3${nnet3_affix}/extractor exp/nnet3${nnet3_affix}/ivectors_${data}_hires
+#####################################################################################################################
+
+#looped decoding
+data_affix=$(echo $data | sed s/test_//)
+nj=$(nproc)
+      
+graph_dir=exp/tri11/graph
+steps/nnet3/decode_looped.sh \
+        --frames-per-chunk 30 \
+        --nj $nj --cmd "$cmd" \
+        --online-ivector-dir exp/nnet3${nnet3_affix}/ivectors_${data}_hires \
+        $graph_dir ${data}_hires ${dir}/decode_looped_${data_affix}
+
+
+
+
+
